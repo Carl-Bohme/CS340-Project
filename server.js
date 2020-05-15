828de14ddf73721a842387849051ed6d4b102305
Carl-Bohme <bohmec@oregonstate.edu>
2020-05-14 21:31:47 -0700

    Starting Files

---
+++ server.js
@@ -0,0 +1,38 @@
+/* #################################
+ Filename: server.js
+ Project: CS 340 Final Project
+ Date: 5/14/2020
+ ################################### */
+
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+
+// Catch for home page
+app.get('/', function (req, res, next) {
+    res.status(200).render('loginPage');
+});
+
+
+// Catch all other requests
+app.get('*', function (req, res) {
+    res.status(404).render('404');
+});
+
+
+// Port Listener
+app.listen(port, function () {
+    console.log("== Server is listening on port", port);
+});
