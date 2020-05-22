4d352a58e021a81533c5f3ac66b7ef49703b406e
croquant <maximedesmet@yahoo.com>
2020-05-22 13:39:51 -0700

    add view pages for bird and subject

--- server.js
+++ server.js
@@ -4,44 +4,60 @@
  Date: 5/14/2020
  ################################### */
 
-var path = require('path');
-var express = require('express');
-var exphbs = require('express-handlebars');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-
+var path = require("path");
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var fs = require("fs");
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.use(express.static('public'));
-
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
 
 // Catch for login page
-app.get('/', function (req, res, next) {
-    res.status(200).render('loginPage');
+app.get("/", function (req, res, next) {
+  res.status(200).render("loginPage");
 });
-
 
 // Catch for home page
-app.get('/home', function (req, res, next) {
-    res.status(200).render('homePage');
+app.get("/home", function (req, res, next) {
+  res.status(200).render("homePage");
 });
 
-app.get('/search/*', function (req, res, next) {
-    res.status(200).render('homePage');
+// Catch for view page
+app.get("/view/bird/", function (req, res, next) {
+  res.status(200).render("viewAllBirdPage");
+});
+
+// Catch for view page
+app.get("/view/bird/:id", function (req, res, next) {
+  res.status(200).render("viewBirdPage");
+});
+
+// Catch for view page
+app.get("/view/subject/", function (req, res, next) {
+  res.status(200).render("viewAllSubjectPage");
+});
+
+// Catch for view page
+app.get("/view/subject/:id", function (req, res, next) {
+  res.status(200).render("viewSubjectPage");
+});
+
+app.get("/search/*", function (req, res, next) {
+  res.status(200).render("homePage");
 });
 
 // Catch all other requests
-app.get('*', function (req, res) {
-    res.status(404).render('404');
+app.get("*", function (req, res) {
+  res.status(404).render("404");
 });
-
 
 // Port Listener
 app.listen(port, function () {
-    console.log("== Server is listening on port", port);
+  console.log("== Server is listening on port", port);
 });
